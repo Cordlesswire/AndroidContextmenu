6eda2e6be411605110e49a749fbd661d3dd3af0f
Delaroy Studios <bamoyk@yahoo.com>
2016-09-15 06:42:25 -0700

    first commit

---
+++ app/src/main/java/com/delaroystudios/contextmenu/MainFragment.java
@@ -0,0 +1,22 @@
+package com.delaroystudios.contextmenu;
+
+/**
+ * Created by delaroystudios on 9/14/2016.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.delaroystudios.contextmenu.R;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
+
